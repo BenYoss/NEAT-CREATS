d81d023ee0e042c59fc8ecead7fcffa1a998e3b4
benyoss <benyoss4@gmail.com>
2021-06-28 10:51:45 -0500

    (add) full configuration of webpack encoder

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  watch: true,
+  entry: path.resolve('./client/src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('./client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['css-loader', 'style-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.sass'],
+  },
+};
