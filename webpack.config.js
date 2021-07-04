4537c914cd869fc94d104fee05123891d2d94347
benyoss <benyoss4@gmail.com>
2021-07-03 21:08:47 -0500

    (update) documentation & loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   watch: true,
   entry: path.resolve('./client/src/Index.jsx'),
   output: {
@@ -12,6 +12,11 @@ module.exports = {
     rules: [
       {
         test: /\.(jsx|js)$/,
+        // node modules should be excluded to prevent regenerator-runtime error.
+        exclude: /node_modules/,
+        // loader helps with additional details on a particular loader.
+        // options can then be used to specify options for the loader
+        // instead of making a separate loader file.
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-react', '@babel/preset-env'],
@@ -20,6 +25,23 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: ['css-loader', 'style-loader', 'sass-loader'],
+      },
+      {
+        // gltf loader helps with rendering WebGL's gltf object files.
+        test: /\.(gltf)$/,
+        use: [
+          {
+            loader: 'gltf-webpack-loader',
+          },
+        ],
+      },
+      {
+        // this helps with the loading of files and with the file loader.
+        test: /\.(png|svg|jpg|gif|bin|jpeg|wav|mp3|eot|ttf|woff|woff2)$/,
+        exclude: /node_modules/,
+        use: [
+          'file-loader',
+        ],
       },
     ],
   },
