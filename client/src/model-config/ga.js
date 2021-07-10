4e131af08e9c35dac1e8a4f093aec7e1973f5620
benyoss <benyoss4@gmail.com>
2021-07-10 11:28:43 -0500

    (add) inheritance patterns betweenc creatures.

--- client/src/model-config/ga.js
+++ client/src/model-config/ga.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-restricted-syntax */
+import faker from 'faker';
 import Creature from './creature';
 
 export function calculateFitness(creatures) {
@@ -12,8 +13,25 @@ export function calculateFitness(creatures) {
 }
 
 export function pickOne(savedCreatures) {
-  const creat = savedCreatures[Math.floor(Math.random() * (savedCreatures.length - 1))];
-  const child = new Creature(creat.brain);
+  // const creat = savedCreatures[Math.floor(Math.random() * (savedCreatures.length - 1))];
+  // const child = new Creature(creat.brain);
+  // child.mutate();
+  // return child;
+
+  let index = 0;
+  let r = Math.random();
+  while (r > 0) {
+    r -= savedCreatures[index].fitness;
+    index += 1;
+  }
+  index -= 1;
+
+  const creature = savedCreatures[index];
+  const child = new Creature(creature.brain, savedCreatures[0].index);
+  child.isCarn = savedCreatures[0].isCarn;
+  child.generation = savedCreatures[0].generation;
+  child.parent = savedCreatures[0].name;
+  child.name = faker.name.findName();
   child.mutate();
   return child;
 }
