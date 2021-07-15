5861d422f81dadab0715033e820c7c5dc554f756
benyoss <benyoss4@gmail.com>
2021-07-14 20:55:17 -0500

    (fix) sass loader in module rules

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
         },
       },
       {
-        test: /\.s[ac]ss$/i,
-        use: ['css-loader', 'style-loader', 'sass-loader'],
+        test: /\.(s[ac]ss)$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         // gltf loader helps with rendering WebGL's gltf object files.
