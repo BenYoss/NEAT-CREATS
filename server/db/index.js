22ff3800ac5da59a3f2059a5e19cd9232584b764
benyoss <benyoss4@gmail.com>
2021-08-15 01:15:22 -0500

    (add) public connection to database

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
+/**
+ * Environmental variables for database information
+ */
+const name = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const cluster = process.env.DB_CLUSTER;
+const dbname = process.env.DB_NAME;
 
 /**
  * connection to mongodb database.
  */
-mongoose.connect('mongodb://localhost/neat-creats', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb+srv://${name}:${pass}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
