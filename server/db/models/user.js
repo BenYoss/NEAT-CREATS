314e5c621d8827f614e24b3538b3ccdb1ac7254b
benyoss <benyoss4@gmail.com>
2021-08-15 01:16:13 -0500

    (add) additional user functions & features

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -2,32 +2,35 @@ require('../index');
 const mongoose = require('mongoose');
 
 // The schema for the User model.
-const UserSchema = mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   id: Number,
-  idUser: Number,
+  idUser: String,
+  isFirst: Boolean,
 });
 
 // creating a new mongoose model instance for User.
-const User = new mongoose.Model(UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 // adds a User to the database.
-const addUser = ({
-  id,
-}) => {
-  const user = new User({
+const addUser = (id) => {
+  const user = {
     idUser: id,
-  });
+    isFirst: false,
+  };
 
-  return user.save();
+  return User.create(user).catch((err) => console.error(err));
 };
 
 // kills a User in the database.
-const killUser = (id) => User.find({ _id: id })
-  .deleteOne().exec().catch((err) => console.error(err));
+const killUser = (id) => User.find({ idUser: id })
+  .deleteMany().then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.error(err));
 
 // updates a User instance with new User data.
-const updateUser = (id, newUser) => User.findOneAndUpdate({ _id: id },
-  { ...newUser })
+const updateUser = (id, bool) => User.findOneAndUpdate({ _id: id },
+  { isFirst: bool })
   .exec().catch((err) => console.error(err));
 
 // get Users from the database.
@@ -35,9 +38,14 @@ const getUser = () => User.find()
   .then((data) => data)
   .catch((err) => console.error(err));
 
+// clear all Users from the database.
+// (NOTE: only meant for cleaning multiple floating records in database)
+const clearUsers = () => User.deleteMany().exec().catch((err) => console.error(err));
+
 module.exports = {
   addUser,
   killUser,
   updateUser,
   getUser,
+  clearUsers,
 };
