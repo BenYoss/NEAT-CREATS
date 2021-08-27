c19e8688e621d87b7c03f1a3f68644190184023a
benyoss <benyoss4@gmail.com>
2021-08-27 15:59:38 -0500

    (remove) hard-coded "mode" option

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  // watch: true,
   entry: path.resolve('./client/src/Index.jsx'),
   output: {
     filename: 'bundle.js',
