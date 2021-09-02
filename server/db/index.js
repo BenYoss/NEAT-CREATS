8d743324c3ebf27fc367bf312584c02326b813ae
benyoss <benyoss4@gmail.com>
2021-09-02 09:34:21 -0500

    (refactor) database to have an "offline" mode.

--- server/db/index.js
+++ server/db/index.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose');
 /**
  * Environmental variables for database information
  */
-const name = process.env.DB_USER;
-const pass = process.env.DB_PASS;
+const name = process.env.DB_USER || 'root';
+const pass = process.env.DB_PASS || '';
 const cluster = process.env.DB_CLUSTER;
 const dbname = process.env.DB_NAME;
 
 /**
  * connection to mongodb database.
  */
-mongoose.connect(`mongodb+srv://${name}:${pass}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_CLUSTER ? `mongodb+srv://${name}:${pass}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority` : 'mongodb://localhost/neat-creats', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
